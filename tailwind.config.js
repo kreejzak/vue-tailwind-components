ebdfe7a10c3ffb60d8bc4efa61519307e075fc30
Paul Cross <me@paulcross.cz>
2021-10-06 13:02:23 +0200

    update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
+  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {},
   },
-  variants: {},
-  plugins: []
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
